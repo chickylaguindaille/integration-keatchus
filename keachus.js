0b7c06d86a3289ddad4faec9aba2b331144c976c
Etienne Leroy <geti.leroy@gmail.com>
2022-02-20 18:59:44 +0100

    ok

--- keachus.js
+++ keachus.js
@@ -6,7 +6,7 @@ let ingredientsSemaine = document.getElementById("btnSemaine");
   listeCourse.addEventListener("click", () => {
   document.getElementById('rubrique').className = "hidden"
   document.getElementById('ingredients').className = "visible"
-  listeCourse.style.backgroundColor = '#c3f3b779';
+  listeCourse.style.backgroundColor = '#212e1d95';
   planning.style.backgroundColor = '#62755B';
   
   })
@@ -15,12 +15,14 @@ let ingredientsSemaine = document.getElementById("btnSemaine");
     document.getElementById('rubrique').className = "visible"
     document.getElementById('ingredients').className = "hidden"
     listeCourse.style.backgroundColor = '#62755B'; //verifier si pas enlevable
-    planning.style.backgroundColor = '#c3f3b779'; //verifier si pas enlevable
+    planning.style.backgroundColor = '#212e1d95'; //verifier si pas enlevable
     document.getElementById('checkbox2').className = "checkbox2"
+    ingredientsSemaine.style.backgroundColor = '#62755B';
     })
 
     ingredientsSemaine.addEventListener("click", () => {
       document.getElementById('checkbox2').className = "checkbox"
+      ingredientsSemaine.style.backgroundColor = '#3c4a36';
       })
 
       /*ajouter un addeventlistener pour reduire liste de course en appuyant sur lundi*/
